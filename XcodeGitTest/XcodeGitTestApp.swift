d4d99581d21f500ffa44199c5d3fbe46d1af606f
kazachilo <kazachilo@gmail.com>
2024-04-11 21:50:30 +0300

    Initial Commit

---
+++ XcodeGitTest/XcodeGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitTestApp.swift
+//  XcodeGitTest
+//
+//  Created by macbook on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
